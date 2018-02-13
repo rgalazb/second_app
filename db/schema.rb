06dcbe7e31383c02c24b655b391e39595066c873
rgalazb <galaz.becerra@gmail.com>
2018-02-13 10:36:34 -0300

    cuarto commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180213130033) do
+ActiveRecord::Schema.define(version: 20180213132811) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20180213130033) do
     t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
